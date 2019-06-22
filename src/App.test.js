e790eb0d8d0896e3ff53d70144f3f832d5460c73
Sean Casey <sean@seanjcasey.com>
2019-06-22 16:44:15 -0400

    start of basic jest tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { Drizzle, generateStore } from "drizzle";
+import { DrizzleContext } from "drizzle-react";
+import drizzleOptions from "./drizzleOptions";
+
+import App from './containers/App';
+
+const drizzleStore = generateStore(drizzleOptions);
+const drizzle = new Drizzle(drizzleOptions, drizzleStore);
+
+describe('App', () => {
+  const drizzleWrappedComponent =
+    (<DrizzleContext.Provider drizzle={drizzle}>
+      <App />
+    </DrizzleContext.Provider>);
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(drizzleWrappedComponent, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(drizzleWrappedComponent);
+    const tree = component.toJSON();
+    console.log(tree)
+    expect(tree).toMatchSnapshot();
+  });
 });
+
+// TODO: End-to-End test
+// Create an order and click vulcanize, want to see specific result (mouse on click vulcanize)
+// - Order added to active orders table?
