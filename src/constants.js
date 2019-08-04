cc83403cc56bbb5112fca31196ba03385c0b8040
Sean Casey <sean@seanjcasey.com>
2019-08-03 20:36:46 -0400

    update token table addresses

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
+// Rinkeby
 export const TOKENTABLE = {
     '0x842b5f2cB93D31AC3A3900AdDA896b223C7A3042': {
         name: 'Sean Token',
@@ -15,15 +16,15 @@ export const TOKENTABLE = {
 
 // Ganache-cli
 // export const TOKENTABLE = {
-//     '0xBd4Ac9375F7cfA025b9BA06A202c7abF24973c40': {
+//     '0x18C1103037a2AF72cAF505619D92a123242bD99E': {
 //         name: 'Sean Token',
 //         symbol: 'SJC'
 //     },
-//     '0x07bF0ED1a0cFffEA8a2A4b8DEA611B79DBdEB0A1': {
+//     '0x3979b72e06C7115F63D95F2E1E50c433ccAe5318': {
 //         name: 'Moon Unit',
 //         symbol: 'MNU'
 //     },
-//     '0x52B001c38739487C1bBEdEE7133E5f302453E64b': {
+//     '0xbeb72a44C0C6e8dF8C69A7B630C95aE03DBfDcD8': {
 //         name: 'Consensys Token',
 //         symbol: 'LUB'
 //     },
